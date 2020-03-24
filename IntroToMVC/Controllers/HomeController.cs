037cb591502839bf6ae5b80cd5cda322f98a4bb2
Tommy Waalkes <Tommy@grandcircus.co>
2020-03-23 20:13:02 -0400

    Added other files

---
+++ IntroToMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IntroToMVC.Models;
+
+namespace IntroToMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Hello = "I want a gordita crunch";
+            return View();
+        }
+
+        public IActionResult Result(Person p)
+        {
+            ViewBag.N = p.Name;
+            ViewBag.A = p.Age;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
